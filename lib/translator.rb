1699f08b4dad3679aa744c08a08ec84bc169e339
Angela Duran <duranangela19@gmail.com>
2018-05-31 21:04:21 -0600

    Redo of timed independent exercse

--- lib/translator.rb
+++ lib/translator.rb
@@ -39,4 +39,29 @@ class Translator
                     "0" => "-----",
                     " " => " "}
   end
+
+  def eng_to_morse(message)
+    message = message.downcase.chars
+    encoded = message.map { |char| @dictionary[char] }
+    encoded.join
+  end
+
+  def from_file(contents)
+    file = File.open(contents, 'r')
+    message = file.read.chomp
+    file.close
+    eng_to_morse(message)
+  end
+
+  def morse_to_eng(message)
+    message_words = message.split('  ')
+    message_split = message_words.map { |word| word.split(' ') }
+    msg_w_spaces = message_split.map { |word| word << " " }
+    decoded_words = message_split.map do |word|
+      decoded = word.map { |char| @dictionary.key(char) }
+    end
+    decoded_words[-1].pop
+    decoded_words.join
+  end
+
 end
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,32 @@
-# YOUR TESTS GO IN HERE!
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator'
+
+class TranslatorTest < Minitest::Test
+
+  def setup
+    @translator = Translator.new
+  end
+
+  def test_it_exists
+    assert_instance_of Translator, @translator
+  end
+
+  def test_it_can_do_eng_to_morse
+    assert_equal "......-...-..--- .-----.-..-..-..", @translator.eng_to_morse("hello world")
+  end
+
+  def test_it_can_translate_case_insensitive_and_with_numbers
+    assert_equal "......-...-..--- .-----.-..-..-..", @translator.eng_to_morse("Hello World")
+    assert_equal "-......-.. .-.-.. ...-- ..........--....", @translator.eng_to_morse("There are 3 ships")
+  end
+
+  def test_it_can_translate_from_a_file
+    assert_equal ".. .--- ..-. .- ..-....-...", @translator.from_file("input.txt")
+  end
+
+  def test_it_can_do_morse_to_eng
+    assert_equal "hello world", @translator.morse_to_eng(".... . .-.. .-.. ---  .-- --- .-. .-.. -..")
+  end
+
+end
